c7131f9c4f98cb5365a9de72ba360c1453ec0c7a
Ayushhrajput <ayushrajput939@gmail.com>
2026-06-23 18:58:49 +0530

    refactor: update getProducts function to handle query parameters and pagination

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,57 @@
-import Product from "../models/product.model";
+import Product from "../models/product.model.js";
 
 const getProducts = async (req, res) => {
+    try {
+        const {
+            category,
+            limit = 10,
+            lastUpdatedAt,
+            lastId      
+        } = req.query
 
+        const query = { }
+
+        if(category) query.category = category
+        
+        if(lastUpdatedAt && lastId) {
+            query.$or = [
+                {
+                    updatedAt: {
+                        $lt: new Date(lastUpdatedAt)
+                    }
+                },
+                {
+                    updatedAt: new Date(lastUpdatedAt),
+                    _id: {
+                        $lt: lastId
+                    }
+                }
+            ]
+        }
+        const products = await Product.find(query)
+            .sort({
+                updatedAt: -1,
+                _id: -1
+            })
+            .limit(Number(limit))
+
+        let nextCursor = null
+
+        if(products.length > 0) {
+            const lastProduct = products[products.length-1]
+
+            nextCursor = {
+                lastUpdatedAt:lastProduct.updatedAt,
+                lastId: lastProduct._id
+            }
+        }
+        return res.json({
+            products,
+            nextCursor
+        })
+
+    } catch (e) {
+        throw new Error(e)
+    }
 }
 export { getProducts }
